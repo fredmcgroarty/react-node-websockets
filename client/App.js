86fc6a538ec496aa6ea4d3a861ac2eab6b8f261a
fredmac <freddymcgroarty@gmail.com>
2020-09-08 19:34:10 +0100

    Adds BOLD as draft.js formatting option

--- client/App.js
+++ client/App.js
@@ -1,17 +1,37 @@
 import React from 'react'
-import { Editor, EditorState, convertToRaw, convertFromRaw } from 'draft-js'
+
+import { 
+  Editor, 
+  EditorState, 
+  RichUtils,
+  convertFromRaw,
+  convertToRaw, 
+} from 'draft-js'
+
 import debounce from 'debounce'
 import 'draft-js/dist/Draft.css';
 
 const j = require('jsondiffpatch')
 
 class App extends React.Component {
-
-  state = {
-    editorState: EditorState.createEmpty()
-  }
   
   _isUnmounted = false
+
+  constructor(props) { 
+    super(props); 
+    this.state = { editorState: EditorState.createEmpty() };
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+  
+  // hijacks keyboard ctrls like cmd+b for bold etc
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return;
+    }
+    return 'not-handled';
+  } 
     
   componentDidMount () {
     this.ws = new window.WebSocket("ws://localhost:1234")
@@ -49,12 +69,18 @@ class App extends React.Component {
     this.setState({ editorState })
   }
 
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+
   render () {
     return (
       <div>
+	<button onClick={this._onBoldClick.bind(this)}>Bold</button>
 	<Editor 
 	  editorState={this.state.editorState}
 	  onChange={this.onChange}
+	  handleKeyCommand={this.handleKeyCommand}
 	/>
       </div>
     )
